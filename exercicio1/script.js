4c2c7303a762187ca258f1809cd386a6305fc7b0
Shiro <aleshirota@gmail.com>
2022-08-29 19:29:35 -0300

    variáveis e tipos primitivos

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+const nome = prompt ("Qual é o seu nome")
+let idade = Number (prompt ("Quantos anos vc tem?"))
+//esse tipo de Variável String e Number foi impresso porque utilizei o comando typeof
+
+console.log (typeof nome)
+console.log (typeof idade)
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos`)
+
---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let Pergunta1 = prompt ("Vc esta usando calça hoje?");
+let Resposta1 = prompt (Pergunta1);
+
+let Pergunta2 = prompt ("Vc vai usar blusa amanhã?");       
+let Resposta2 = prompt (Pergunta2); 
+
+let Pergunta3 = prompt ("Vc está usando sapato hoje?");
+let Resposta3 = prompt (Pergunta3);
+
+
+console.log(`Vc esta usando calça hoje? ${Pergunta1} Vc vai usar blusa amanhã? ${Pergunta2} Vc está usando sapato hoje? ${Pergunta3}`);
+
